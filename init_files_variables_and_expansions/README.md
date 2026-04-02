ff39214a5e52ce223aa783aa98ef4d233066bdaa
Elvire Akayezu <e.akayezu@alustudent.com>
2026-04-02 00:18:04 -0700

    Added file 0-aliasto the repo

---
+++ init_files_variables_and_expansions/README.md
@@ -0,0 +1,2 @@
+#!/bin/ash
+In this drectory we will make many files with scripts to apply the shell initial variables and expansions.
---
+++ init_files_variables_and_expansions/ans_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "Enter the file name : " file_name
+read -p "Enter the command answer : " cmd
+
+echo -e "#!/bin/bash" > ${file_name}
+echo -e "${cmd}" >> ${file_name}
+
+chmod u+x ${file_name}
+
+git add .
+
+git commit -m "Added file ${file_name}to the repo"
+
+git push
+
