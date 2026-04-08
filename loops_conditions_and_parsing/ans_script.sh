c57db834e958160f7e81fe7ef8c80f9124fc52ce
Elvire Akayezu <e.akayezu@alustudent.com>
2026-04-08 04:26:08 -0700

    Added file 6-superstitious_numbers to the repo

--- loops_conditions_and_parsing/ans_script.sh
+++ loops_conditions_and_parsing/ans_script.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 read -p "Enter the file name : " file_name
-read -p "Type what your script is going to do, rmbr to START with \"\#\": " comment
-echo -e "Enter the command answer \(Press Ctrl+D when done\): "
-cmd=""
-while IFS= read -r line; do
-	cmd="${cmd}${line}"$'\n'
-done
+read -p "Type what your script is going to do, rmbr to START with \"#\": " comment
+echo -e "Enter the command answer (Press Ctrl+D when done): "
+
+tmp_file=$(mktemp)
+vi "$tmp_file"
+cmd=$(cat "$tmp_file")
+rm "$tmp_file"
+
+#cmd=""
+#while IFS= read -r line; do
+	#cmd="${cmd}${line}"$'\n'
+#done
 
 echo -e "#!/usr/bin/env bash" > ${file_name}
 echo -e ${comment} >> ${file_name}
